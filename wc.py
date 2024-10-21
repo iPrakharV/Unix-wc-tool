fcb697a023b1766e6e417e685c493e65bbe9c4e8
Prakhar Verma <connect@iprakharv.com>
2024-10-21 13:17:58 -0400

    main complete
    Signed-off-by: Prakhar Verma <connect@iprakharv.com>

--- wc.py
+++ wc.py
@@ -7,3 +7,13 @@ def main():
     parser = argparse.ArgumentParser(description='Unix WC Tool')
     parser.add_argument('-c', action='store_true', help='Count Bytes')
     parser.add_argument('file', help='File to process')
+
+    args = parser.parse_args()
+    with open(args.file, 'r') as file:
+        content = file.read()
+
+    if args.c:
+        print(f'Bytes: {count_bytes(content)}')
+
+if __name__ == '__main__':
+    main()
