dc3785effbd484070b3c8fa84f64a045f63dc414
Prakhar Verma <connect@iprakharv.com>
2024-10-21 13:15:53 -0400

    Manages CLI
    Signed-off-by: Prakhar Verma <connect@iprakharv.com>

--- wc.py
+++ wc.py
@@ -1,4 +1,9 @@
 import argparse
 
-def count_bytes(file)
+def count_bytes(file):
     return len(file.encode('utf-8'))
+
+def main():
+    parser = argparse.ArgumentParser(description='Unix WC Tool')
+    parser.add_argument('-c', action='store_true', help='Count Bytes')
+    parser.add_argument('file', help='File to process')
